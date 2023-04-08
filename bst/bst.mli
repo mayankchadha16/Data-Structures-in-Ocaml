92e8e6f76aa6831c1986e3e20f6cd234563405ba
mayankchadha16 <chadhamayank1609@gmail.com>
2023-04-08 12:20:00 +0530

    Add modularity

---
+++ bst/bst.mli
@@ -0,0 +1,15 @@
+module MyBST :
+sig
+    type 'a tree = Node of 'a * 'a tree * 'a tree | Leaf
+    val empty : 'a tree
+    exception Found
+    val mem : 'a -> 'a tree -> bool
+    val member_opt : 'a -> 'a tree -> bool
+    val insert : 'a -> 'a tree -> 'a tree
+    val insert_opt : 'a -> 'a tree -> 'a tree
+    val height : 'a tree -> int
+    val count : 'a tree -> int
+    val complete : 'a -> int -> 'a tree
+end
+
+val test_case : unit -> unit
---
+++ bst/driver.ml
@@ -0,0 +1,1 @@
+let run = Bst.test_case ()
